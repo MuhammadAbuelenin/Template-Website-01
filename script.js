940d98d3e3bb50a6b6f61a5195eecbba00f6faf3
Muhammad Abu-Elenin <76665270+MuhammadAbuelenin@users.noreply.github.com>
2021-03-23 00:57:48 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// // $(function () {
+    
+// //     $(".featured-work ul li").on("click", function () {
+// //         $(this).addClass("active").siblings().removeClass("active");
+// //     });
+// // });
+// $(function () {
+    
+//     $('.featured-work ul li').on('click', function () {
+//         $(this).addClass('active').siblings().removeClass('active');
+//     });
+// });
